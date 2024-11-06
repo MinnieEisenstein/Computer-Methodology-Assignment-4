6f7316a566e09f1fadc3e383abc6025e11d70736
MinnieEisenstein <meisenst3@student.touro.edu>
2024-11-05 19:30:33 -0500

    fixed up mock object

--- src/assignment4/MockRandomValueGenerator.java
+++ src/assignment4/MockRandomValueGenerator.java
@@ -8,15 +8,20 @@ public class MockRandomValueGenerator implements IRandomValueGenerator{
 	
 	@Override
 	public int getRandomNum(int min, int max) {
-		return 5;//will always return 5
+		return randomNum;//will always return set randomNum
 		
 	}
 
 	@Override
 	public boolean getTrueWithProbability(double p) {
-		 return false;//will always return false
+		 return probabilityResult;//will always return same boolean taht is set
 	}
 	
+	public void setRandomNum(int num) {
+		this.randomNum= num;
+	}
 
-	
+	public void setProbabilityResult(boolean result) {
+		this.probabilityResult= result;
+	}
 }
